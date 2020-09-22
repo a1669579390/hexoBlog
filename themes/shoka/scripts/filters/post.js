b6ac53e0c0345df7a4eec768da54f5ffac9936d9
1669579390@qq.com <1669579390@qq.com>
2020-09-22 19:02:06 +0800

    修正表情和头像的显示问题

--- themes/shoka/scripts/filters/post.js
+++ themes/shoka/scripts/filters/post.js
@@ -1,24 +1,32 @@
 /* global hexo */
 
-'use strict';
+"use strict";
 
-hexo.extend.filter.register('after_post_render', data => {
-  const { config } = hexo;
-  const theme = hexo.theme.config;
+hexo.extend.filter.register(
+  "after_post_render",
+  (data) => {
+    const { config } = hexo;
+    const theme = hexo.theme.config;
+    //之前下方的data-src 导致表情无法显示 所以改为src
+    data.content = data.content.replace(/(<img[^>]*) src=/gim, "$1 src=");
 
-  data.content = data.content.replace(/(<img[^>]*) src=/img, '$1 data-src=');
+    const url = require("url");
+    const siteHost = url.parse(config.url).hostname || config.url;
+    data.content = data.content.replace(
+      /<a[^>]* href="([^"]+)"[^>]*>([^<]*)<\/a>/gim,
+      (match, href, html) => {
+        // Exit if the href attribute doesn't exists.
+        if (!href) return match;
 
-  const url = require('url');
-  const siteHost = url.parse(config.url).hostname || config.url;
-  data.content = data.content.replace(/<a[^>]* href="([^"]+)"[^>]*>([^<]*)<\/a>/img, (match, href, html) => {
-    // Exit if the href attribute doesn't exists.
-    if (!href) return match;
+        // Exit if the url has same host with `config.url`, which means it's an internal link.
+        let link = url.parse(href);
+        if (!link.protocol || link.hostname === siteHost) return match;
 
-    // Exit if the url has same host with `config.url`, which means it's an internal link.
-    let link = url.parse(href);
-    if (!link.protocol || link.hostname === siteHost) return match;
-
-    return `<span class="exturl" data-url="${Buffer.from(href).toString('base64')}">${html}</span>`;
-  });
-
-}, 0);
+        return `<span class="exturl" data-url="${Buffer.from(href).toString(
+          "base64"
+        )}">${html}</span>`;
+      }
+    );
+  },
+  0
+);
