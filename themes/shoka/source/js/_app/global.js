4eb00e2f4e1f7941c0ca9d5552ff5cb4b15c3898
yangtongmiao <1669579390@qq.com>
2021-09-24 16:42:08 +0800

    新增触发配置loading选项

--- themes/shoka/source/js/_app/global.js
+++ themes/shoka/source/js/_app/global.js
@@ -1,188 +1,197 @@
-var statics = CONFIG.statics.indexOf('//') > 0 ? CONFIG.statics : CONFIG.root
-var scrollAction = { x: 'undefined', y: 'undefined' };
+var statics = CONFIG.statics.indexOf("//") > 0 ? CONFIG.statics : CONFIG.root;
+var scrollAction = { x: "undefined", y: "undefined" };
 var diffY = 0;
 var originTitle, titleTime;
 
-const BODY = document.getElementsByTagName('body')[0];
+const BODY = document.getElementsByTagName("body")[0];
 const HTML = document.documentElement;
-const Container = $('#container');
-const loadCat = $('#loading');
-const siteNav = $('#nav');
-const siteHeader = $('#header');
-const menuToggle = siteNav.child('.toggle');
-const quickBtn = $('#quick');
-const sideBar = $('#sidebar');
-const siteBrand = $('#brand');
-var toolBtn = $('#tool'), toolPlayer, backToTop, goToComment, showContents;
-var siteSearch = $('#search');
+const Container = $("#container");
+const loadCat = $("#loading");
+const siteNav = $("#nav");
+const siteHeader = $("#header");
+const menuToggle = siteNav.child(".toggle");
+const quickBtn = $("#quick");
+const sideBar = $("#sidebar");
+const siteBrand = $("#brand");
+var toolBtn = $("#tool"),
+  toolPlayer,
+  backToTop,
+  goToComment,
+  showContents;
+var siteSearch = $("#search");
 var siteNavHeight, headerHightInner, headerHight;
 
 const Loader = {
   timer: null,
   lock: false,
-  show: function() {
+  show: function () {
     clearTimeout(this.timer);
-    document.body.removeClass('loaded');
+    document.body.removeClass("loaded");
     Velocity(loadCat, "fadeIn", {
-      complete: function() {
+      complete: function () {
         Loader.lock = false;
-      }
+      },
     });
   },
-  hide: function(sec) {
-    this.timer = setTimeout(this.vanish, sec||3000);
+  hide: function (sec) {
+    this.timer = setTimeout(this.vanish, sec || 3000);
   },
-  vanish: function() {
-    if(Loader.lock)
-      return;
+  vanish: function () {
+    if (Loader.lock) return;
     Velocity(loadCat, "fadeOut");
-    document.body.addClass('loaded');
+    document.body.addClass("loaded");
     Loader.lock = true;
-  }
-}
+  },
+};
 
-const changeTheme = function(type) {
-  var btn = $('.theme .ic')
-  if(type) {
-    HTML.attr('data-theme', type);
-    btn.removeClass('i-sun')
-    btn.addClass('i-moon')
+const changeTheme = function (type) {
+  var btn = $(".theme .ic");
+  if (type) {
+    HTML.attr("data-theme", type);
+    btn.removeClass("i-sun");
+    btn.addClass("i-moon");
   } else {
-    HTML.attr('data-theme', null);
-    btn.removeClass('i-moon');
-    btn.addClass('i-sun');
+    HTML.attr("data-theme", null);
+    btn.removeClass("i-moon");
+    btn.addClass("i-sun");
   }
-}
+};
 
-const changeMetaTheme = function(color) {
-  if(HTML.attr('data-theme') == 'dark')
-    color = '#222'
+const changeMetaTheme = function (color) {
+  if (HTML.attr("data-theme") == "dark") color = "#222";
 
-  $('meta[name="theme-color"]').attr('content', color);
-}
+  $('meta[name="theme-color"]').attr("content", color);
+};
 
 const themeColorListener = function () {
-  window.matchMedia('(prefers-color-scheme: dark)').addListener(function(mediaQueryList) {
-    if(mediaQueryList.matches){
-      changeTheme('dark');
-    } else {
-      changeTheme();
-    }
-  });
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addListener(function (mediaQueryList) {
+      if (mediaQueryList.matches) {
+        changeTheme("dark");
+      } else {
+        changeTheme();
+      }
+    });
 
-  var t = store.get('theme');
-  if(t) {
+  var t = store.get("theme");
+  if (t) {
     changeTheme(t);
   }
 
-  $('.theme').addEventListener('click', function(event) {
-    var btn = event.currentTarget.child('.ic')
+  $(".theme").addEventListener("click", function (event) {
+    var btn = event.currentTarget.child(".ic");
 
-    var neko = BODY.createChild('div', {
-      id: 'neko',
-      innerHTML: '<div class="planet"><div class="sun"></div><div class="moon"></div></div><div class="body"><div class="face"><section class="eyes left"><span class="pupil"></span></section><section class="eyes right"><span class="pupil"></span></section><span class="nose"></span></div></div>'
+    var neko = BODY.createChild("div", {
+      id: "neko",
+      innerHTML:
+        '<div class="planet"><div class="sun"></div><div class="moon"></div></div><div class="body"><div class="face"><section class="eyes left"><span class="pupil"></span></section><section class="eyes right"><span class="pupil"></span></section><span class="nose"></span></div></div>',
     });
 
-    var hideNeko = function() {
-      setTimeout(function() {
+    var hideNeko = function () {
+      setTimeout(function () {
         Velocity(neko, "fadeOut", {
-          complete: function() {
-            BODY.removeChild(neko)
-          }
+          complete: function () {
+            BODY.removeChild(neko);
+          },
         });
       }, 2500);
-    }
+    };
 
-    if(btn.hasClass('i-sun')) {
+    if (btn.hasClass("i-sun")) {
       Velocity(neko, "fadeIn", {
-        complete: function() {
-          neko.addClass('dark');
-          changeTheme('dark');
-          store.set('theme', 'dark');
+        complete: function () {
+          neko.addClass("dark");
+          changeTheme("dark");
+          store.set("theme", "dark");
           hideNeko();
-        }
+        },
       });
     } else {
-      neko.addClass('dark');
+      neko.addClass("dark");
       Velocity(neko, "fadeIn", {
-        complete: function() {
-          neko.removeClass('dark');
+        complete: function () {
+          neko.removeClass("dark");
           changeTheme();
-          store.del('theme');
+          store.del("theme");
           hideNeko();
-        }
+        },
       });
     }
   });
-
-}
+};
 
 const visibilityListener = function () {
-  document.addEventListener('visibilitychange', function() {
-    switch(document.visibilityState) {
-      case 'hidden':
-        $('[rel="icon"]').attr('href', statics + CONFIG.favicon.hidden);
+  document.addEventListener("visibilitychange", function () {
+    switch (document.visibilityState) {
+      case "hidden":
+        $('[rel="icon"]').attr("href", statics + CONFIG.favicon.hidden);
         document.title = LOCAL.favicon.hide;
-        Loader.show()
+        CONFIG.customConfig.loader && Loader.show();
         clearTimeout(titleTime);
-      break;
-      case 'visible':
-        $('[rel="icon"]').attr('href', statics + CONFIG.favicon.normal);
+        break;
+      case "visible":
+        $('[rel="icon"]').attr("href", statics + CONFIG.favicon.normal);
         document.title = LOCAL.favicon.show;
-        Loader.hide(1000)
+        CONFIG.customConfig.loader && Loader.hide(1000);
         titleTime = setTimeout(function () {
           document.title = originTitle;
         }, 2000);
-      break;
+        break;
     }
   });
-}
+};
 
-const showtip = function(msg) {
-  if(!msg)
-    return
+const showtip = function (msg) {
+  if (!msg) return;
 
-  var tipbox = BODY.createChild('div', {
+  var tipbox = BODY.createChild("div", {
     innerHTML: msg,
-    className: 'tip'
+    className: "tip",
   });
 
-  setTimeout(function() {
-    tipbox.addClass('hide')
-    setTimeout(function() {
+  setTimeout(function () {
+    tipbox.addClass("hide");
+    setTimeout(function () {
       BODY.removeChild(tipbox);
     }, 300);
   }, 3000);
-}
+};
 
 const resizeHandle = function (event) {
   siteNavHeight = siteNav.height();
   headerHightInner = siteHeader.height();
-  headerHight = headerHightInner + $('#waves').height();
+  headerHight = headerHightInner + $("#waves").height();
 
   sideBarToggleHandle(null, 1);
-  sideBar.style = '';
-}
+  sideBar.style = "";
+};
 
 const scrollHandle = function (event) {
   var winHeight = window.innerHeight;
   var docHeight = getDocHeight();
-  var contentVisibilityHeight = docHeight > winHeight ? docHeight - winHeight : document.body.scrollHeight - winHeight;
+  var contentVisibilityHeight =
+    docHeight > winHeight
+      ? docHeight - winHeight
+      : document.body.scrollHeight - winHeight;
   var SHOW = window.pageYOffset > headerHightInner;
   var startScroll = window.pageYOffset > 0;
 
   if (SHOW) {
-    changeMetaTheme('#FFF');
+    changeMetaTheme("#FFF");
   } else {
-    changeMetaTheme('#222');
+    changeMetaTheme("#222");
   }
 
-  siteNav.toggleClass('show', SHOW);
-  toolBtn.toggleClass('affix', startScroll);
-  siteBrand.toggleClass('affix', startScroll);
-  sideBar.toggleClass('affix', window.pageYOffset > headerHight && document.body.offsetWidth > 991);
+  siteNav.toggleClass("show", SHOW);
+  toolBtn.toggleClass("affix", startScroll);
+  siteBrand.toggleClass("affix", startScroll);
+  sideBar.toggleClass(
+    "affix",
+    window.pageYOffset > headerHight && document.body.offsetWidth > 991
+  );
 
-  if (typeof scrollAction.y == 'undefined') {
+  if (typeof scrollAction.y == "undefined") {
     scrollAction.y = window.pageYOffset;
     //scrollAction.x = Container.scrollLeft;
     //scrollAction.y = Container.scrollTop;
@@ -197,49 +206,52 @@ const scrollHandle = function (event) {
   //} else
   if (diffY < 0) {
     // Scroll down
-    siteNav.removeClass('up')
-    siteNav.toggleClass('down', SHOW);
+    siteNav.removeClass("up");
+    siteNav.toggleClass("down", SHOW);
   } else if (diffY > 0) {
     // Scroll up
-    siteNav.removeClass('down')
-    siteNav.toggleClass('up', SHOW);
+    siteNav.removeClass("down");
+    siteNav.toggleClass("up", SHOW);
   } else {
     // First scroll event
   }
   //scrollAction.x = Container.scrollLeft;
   scrollAction.y = window.pageYOffset;
 
-  var scrollPercent = Math.round(Math.min(100 * window.pageYOffset / contentVisibilityHeight, 100)) + '%';
-  backToTop.child('span').innerText = scrollPercent;
-  $('.percent').width(scrollPercent);
-}
+  var scrollPercent =
+    Math.round(
+      Math.min((100 * window.pageYOffset) / contentVisibilityHeight, 100)
+    ) + "%";
+  backToTop.child("span").innerText = scrollPercent;
+  $(".percent").width(scrollPercent);
+};
 
-const pagePostion = function(url) {
-  store.set(url, scrollAction.y)
-}
+const pagePostion = function (url) {
+  store.set(url, scrollAction.y);
+};
 
-const postionInit = function() {
-  var anchor = window.location.hash
-  if(anchor) {
+const postionInit = function () {
+  var anchor = window.location.hash;
+  if (anchor) {
     pageScroll($(decodeURI(anchor)));
   } else {
-    var position = store.get(window.location.href)
-    if(position) {
+    var position = store.get(window.location.href);
+    if (position) {
       pageScroll(BODY, position);
       store.del(window.location.href);
     }
   }
-}
+};
 
-const clipBoard = function(str, callback) {
-  var ta = BODY.createChild('textarea', {
+const clipBoard = function (str, callback) {
+  var ta = BODY.createChild("textarea", {
     style: {
-      top: window.scrollY + 'px', // Prevent page scrolling
-      position: 'absolute',
-      opacity: '0'
+      top: window.scrollY + "px", // Prevent page scrolling
+      position: "absolute",
+      opacity: "0",
     },
     readOnly: true,
-    value: str
+    value: str,
   });
 
   const selection = document.getSelection();
@@ -247,7 +259,7 @@ const clipBoard = function(str, callback) {
   ta.select();
   ta.setSelectionRange(0, str.length);
   ta.readOnly = false;
-  var result = document.execCommand('copy');
+  var result = document.execCommand("copy");
   callback && callback(result);
   ta.blur(); // For iOS
   if (selected) {
@@ -255,4 +267,4 @@ const clipBoard = function(str, callback) {
     selection.addRange(selected);
   }
   BODY.removeChild(ta);
-}
+};
